a647c69f98d797bb5d3fcda0df04eb7fd4abf0e3
tsumina <liu.chang.1865@gmail.com>
2018-12-18 02:54:40 +0900

    fix test in CI

--- tests/datatools/test_datatools.py
+++ tests/datatools/test_datatools.py
@@ -20,7 +20,8 @@ def setup():
         fetch_file=
         'https://raw.githubusercontent.com/yoshida-lab/XenonPy/master/travis/fetch_test.txt',
         test_dir='test_dir',
-        user_dataset='test_user_data')
+        user_dataset='test_user_data',
+        saver=LocalStorage('test_user_data'))
 
     # ignore numpy warning
     import warnings
@@ -66,20 +67,20 @@ def test_preset_property():
 
 # =====================================================================================================
 def test_local_storage1(setup):
-    saver = LocalStorage(setup['user_dataset'])
+    saver = setup['saver']
     ret = '<{}> include:'.format(setup['user_dataset'])
     assert str(saver) == ret, 'no files'
 
 
 def test_local_storage2(setup):
-    saver = LocalStorage(setup['user_dataset'])
+    saver = setup['saver']
     saver(list('abcd'), list('efgh'))
     print('lcoal_storage2', saver)
     assert len(saver._files['unnamed']) == 2, 'should got 2 files'
 
 
 def test_local_storage3(setup):
-    saver = LocalStorage(setup['user_dataset'])
+    saver = setup['saver']
     saver(key1=list('asdf'), key2=list('qwer'))
     assert len(saver._files['unnamed']) == 2, 'should got 2 files'
     assert len(saver._files['key1']) == 1, 'should got 1 files'
@@ -87,7 +88,7 @@ def test_local_storage3(setup):
 
 
 def test_local_storage4(setup):
-    saver = LocalStorage(setup['user_dataset'])
+    saver = setup['saver']
     saver(list('asdf'), key1=list('qwer'))
     print('lcoal_storage4', saver)
     assert len(saver._files['unnamed']) == 3, 'should got 3 files'
@@ -96,25 +97,25 @@ def test_local_storage4(setup):
 
 
 def test_local_storage_prop(setup):
-    saver = LocalStorage(setup['user_dataset'])
+    saver = setup['saver']
     assert saver.name == 'test_user_data'
     assert saver.path == str(Path.home() / '.xenonpy' / 'userdata')
 
 
 def test_local_storage_last1(setup):
-    saver = LocalStorage(setup['user_dataset'])
+    saver = setup['saver']
     last = saver.last()
     assert last == list('asdf'), 'retriever same data'
 
 
 def test_local_storage_last2(setup):
-    saver = LocalStorage(setup['user_dataset'])
+    saver = setup['saver']
     last = saver.last('key1')
     assert last == list('qwer'), 'retriever same data'
 
 
 def test_local_storage_getitem1(setup):
-    saver = LocalStorage(setup['user_dataset'])
+    saver = setup['saver']
     item = saver[:]
     print(item)
     assert item[1] == list('efgh'), 'retriever same data'
@@ -123,7 +124,7 @@ def test_local_storage_getitem1(setup):
 
 
 def test_local_storage_getitem2(setup):
-    saver = LocalStorage(setup['user_dataset'])
+    saver = setup['saver']
     item = saver['key2', :]
     assert item[0] == list('qwer'), 'retriever same data'
     item = saver['key1', 1]
@@ -131,7 +132,7 @@ def test_local_storage_getitem2(setup):
 
 
 def test_dump1(setup):
-    saver = LocalStorage(setup['user_dataset'])
+    saver = setup['saver']
     path = Path().expanduser()
     path_ = saver.dump(path, with_datetime=False)
     assert path_ == str(path / (setup['user_dataset'] + '.pkl.z'))
@@ -145,19 +146,19 @@ def test_dump2(setup):
 
 
 def test_local_storage_chained(setup):
-    saver = LocalStorage(setup['user_dataset']).chain
+    saver = setup['saver'].chain
     assert saver.name == 'chain'
     assert saver.path == str(Path.home() / '.xenonpy' / 'userdata' / 'test_user_data')
 
 
 def test_local_storage_delete1(setup):
-    saver = LocalStorage(setup['user_dataset'])
+    saver = setup['saver']
     saver.rm(0)
     assert len(saver._files['unnamed']) == 2, 'should got 1 files'
 
 
 def test_local_storage_delete2(setup):
-    saver = LocalStorage(setup['user_dataset'])
+    saver = setup['saver']
     saver(key1=list('qwer'))
     assert len(saver._files['key1']) == 3, 'should got 3 files'
     saver.rm(slice(0, 2), 'key1')
@@ -165,13 +166,13 @@ def test_local_storage_delete2(setup):
 
 
 def test_local_storage_clean1(setup):
-    saver = LocalStorage(setup['user_dataset'])
+    saver = setup['saver']
     saver.clean('key1')
     assert 'key1' not in saver._files, 'no saver dir'
 
 
 def test_local_storage_clean2(setup):
-    saver = LocalStorage(setup['user_dataset'])
+    saver = setup['saver']
     saver_dir = Path.home() / '.xenonpy' / 'userdata' / setup['user_dataset']
     saver.clean()
     assert not saver_dir.exists(), 'no saver dir'

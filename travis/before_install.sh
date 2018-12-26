60bf2fe6cc97e3fb9e6a9c604614256c21998c56
tsumina <liu.chang.1865@gmail.com>
2018-12-26 12:29:09 +0900

    fix

--- travis/before_install.sh
+++ travis/before_install.sh
@@ -53,6 +53,6 @@ python -c "import scipy; print('scipy %s' % scipy.__version__)"
 python -c "import pandas; print('pandas %s' % pandas.__version__)"
 python -c "import torch; print('pytorch %s' % torch.__version__)"
 python -c "import rdkit; print('rdkit %s' % rdkit.__version__)"
-python -c "import pymatgen print('pymatgen %s' % pymatgen.__version__)"
+python -c "import pymatgen; print('pymatgen %s' % pymatgen.__version__)"
 
 #python setup.py install

7c3dd4104cbfc3d3123bc70253390e776be6cfb1
TsumiNa <liu.chang.1865@gmail.com>
2019-02-05 12:00:10 +0900

    close #46; checkpoint

--- xenonpy/descriptor/fingerprint.py
+++ xenonpy/descriptor/fingerprint.py
@@ -1,3 +1,7 @@
+#  Copyright 2019. TsumiNa. All rights reserved.
+#  Use of this source code is governed by a BSD-style
+#  license that can be found in the LICENSE file.
+
 from rdkit.Chem import Descriptors
 from rdkit import Chem
 from rdkit.Chem import MACCSkeys as MAC

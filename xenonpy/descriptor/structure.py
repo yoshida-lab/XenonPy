be9418025118ea7b7166f4591b0a308d03f56326
TsumiNa <liu.chang.1865@gmail.com>
2019-02-05 12:00:10 +0900

    close #46; checkpoint

--- xenonpy/descriptor/structure.py
+++ xenonpy/descriptor/structure.py
@@ -1,4 +1,4 @@
-# Copyright 2017 TsumiNa. All rights reserved.
+# Copyright 2018 TsumiNa. All rights reserved.
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
 

c89fda8ee09cabbc48ca0ee2aa110991d2e4f49d
TsumiNa <liu.chang.1865@gmail.com>
2019-02-05 12:00:10 +0900

    close #46; checkpoint

--- xenonpy/descriptor/frozen_featurizer.py
+++ xenonpy/descriptor/frozen_featurizer.py
@@ -1,3 +1,7 @@
+#  Copyright 2019. TsumiNa. All rights reserved.
+#  Use of this source code is governed by a BSD-style
+#  license that can be found in the LICENSE file.
+
 import numpy as np
 import pandas as pd
 import torch as tc

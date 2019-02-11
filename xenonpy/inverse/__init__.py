9a3823e14c3f9adf89cf5df66952c4e6d416ec85
tsumina <liu.chang.1865@gmail.com>
2019-02-11 13:55:48 +0900

    checkpoint

--- xenonpy/inverse/__init__.py
+++ xenonpy/inverse/__init__.py
@@ -2,7 +2,7 @@
 #  Use of this source code is governed by a BSD-style
 #  license that can be found in the LICENSE file.
 
-__all__ = ['BaseSMC', 'IQSPR']
+__all__ = ['BaseSMC', 'IQSPR', 'BaseProposer', 'BaseLogLikelihood']
 
-from .base import BaseSMC
+from .base_smc import BaseSMC, BaseLogLikelihood, BaseProposer
 from .iqspr import IQSPR

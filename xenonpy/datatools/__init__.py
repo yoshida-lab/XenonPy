1f8496d61d172a6970ab94ea853c782cb2ff6ed3
tsumina <liu.chang.1865@gmail.com>
2019-02-19 19:12:47 +0900

    check point

--- xenonpy/datatools/__init__.py
+++ xenonpy/datatools/__init__.py
@@ -2,8 +2,9 @@
 #  Use of this source code is governed by a BSD-style
 #  license that can be found in the LICENSE file.
 
-__all__ = ['LocalStorage', 'Preset', 'preset', 'Splitter', 'Scaler', 'BoxCox']
+__all__ = ['LocalStorage', 'Preset', 'preset', 'Splitter', 'Scaler', 'BoxCox', 'MDL']
 
 from .dataset import LocalStorage, Preset, preset
+from .mdl import MDL
 from .preprocess import Splitter
 from .transform import Scaler, BoxCox
--- xenonpy/datatools/mdl.py
+++ xenonpy/datatools/mdl.py
@@ -4,24 +4,187 @@
 
 # %%
 import json
+import os
+import tarfile
+from pathlib import Path
 
+import pandas as pd
 import requests
+from requests import HTTPError
+from sklearn.base import BaseEstimator
+from tqdm import tqdm
+
+from ..utils import TimedMetaClass, absolute_path
 
 
-def get_properties(name):
-    # url = 'http://xenon.ism.ac.jp/api'
-    url = 'http://localhost:3001/api'
-    headers = {'Accept': 'application/json', "content-type": "application/json"}
-    query = '''
-    query ($name: String!){
-      queryProperties(name: $name){
-        name
-        models
-      }
-    }
-    '''
-    variables = {"name": name}
-    payload = json.dumps({'query': query, 'variables': variables})
+class MDL(BaseEstimator, metaclass=TimedMetaClass):
+    def __init__(self, *, save_to='.', api_key=''):
+        """
+        Access to XenonPy.MDL.
 
-    ret = requests.post(url=url, headers=headers, data=payload)
-    return ret
+        Parameters
+        ----------
+        save_to: str
+            Path to save models.
+        api_key: str
+            Not implement yet.
+        """
+        self._save_to = save_to
+        self._url = 'http://xenon.ism.ac.jp/api'
+        self._api_key = api_key
+        self._headers = {'Accept': 'application/json', "content-type": "application/json"}
+
+    def query(self, query, variables):
+        payload = json.dumps({'query': query, 'variables': variables})
+        ret = requests.post(url=self._url, headers=self._headers, data=payload)
+        if ret.status_code != 200:
+            raise HTTPError('status_code: %s, %s' %
+                            (ret.status_code, ret.json()))
+        ret = ret.json()['data']
+        return ret
+
+    def query_properties(self, name_has, *,
+                         query_fullname=False,
+                         query_symbol=False,
+                         query_models=False,
+                         query_unit=False):
+        query_fullname = 'fullname' if query_fullname else ''
+        query_symbol = 'symbol' if query_symbol else ''
+        query_unit = 'unit' if query_unit else ''
+        query_models = 'models' if query_models else ''
+        query = '''
+        query ($name: String!){
+          queryProperties(name: $name){
+            name
+            %s
+            %s
+            %s
+            %s
+          }
+        }
+        ''' % (query_fullname, query_symbol, query_unit, query_models)
+
+        variables = {"name": name_has}
+        return self.query(query, variables)['queryProperties']
+
+    def _query_models(self, modelset_has, *,
+                      property_has='',
+                      descriptor_has='',
+                      method_has='',
+                      lang_has='',
+                      regress_is=True,
+                      transferred_is=False,
+                      succeed_is=True):
+        query = '''
+        query ($modelSet: String!
+               $property: String
+               $descriptor: String
+               $method: String
+               $lang: String
+               $regress: Boolean
+               $transferred: Boolean
+               $succeed: Boolean)
+        {
+          queryModels(modelSet: $modelSet
+               property: $property
+               descriptor: $descriptor
+               method: $method
+               lang: $lang
+               regress: $regress
+               transferred: $transferred
+               succeed: $succeed)
+          {
+            id
+            url
+            modelSet
+            property
+            descriptor
+            method
+            lang
+            regress
+            transferred
+            succeed
+          }
+        }
+        '''
+
+        variables = dict(
+            modelSet=modelset_has,
+            property=property_has,
+            descriptor=descriptor_has,
+            method=method_has,
+            lang=lang_has,
+            regress=regress_is,
+            transferred=transferred_is,
+            succeed=succeed_is
+        )
+        return self.query(query, variables)['queryModels']
+
+    def __call__(self, modelset_has, save=True, *,
+                 property_has='',
+                 descriptor_has='',
+                 method_has='',
+                 lang_has='',
+                 regress_is=True,
+                 transferred_is=False,
+                 succeed_is=True):
+        """
+        Query models and download to a specific destination
+
+        Parameters
+        ----------
+        modelset_has: str
+            The part of a model set's name.
+            For example, ``modelset_has='test`` will hit ``*test*``
+        property_has: str
+            A part of the name of property.
+        descriptor_has: str
+            A part of the name of descriptor.
+        method_has: str
+            A part of the name of training method.
+        lang_has: str
+            A part of the name of programming language.
+        regress_is: bool
+            If``True``, searching in regression models,
+            else, searching in classification models.
+            Default is ``True``.
+        transferred_is: bool
+            If ``True``, searching in transferred models.
+            Default is ``False``.
+        succeed_is: bool
+            If ``True``, searching in succeed models.
+            Default is ``True``.
+
+        Returns
+        -------
+        ret: pd.DataFrame
+            A summary of all downloaded models.
+        """
+        ret = self._query_models(modelset_has,
+                                 property_has=property_has,
+                                 descriptor_has=descriptor_has,
+                                 method_has=method_has,
+                                 lang_has=lang_has,
+                                 regress_is=regress_is,
+                                 transferred_is=transferred_is,
+                                 succeed_is=succeed_is)
+        if not ret:
+            return None
+        ret_ = pd.DataFrame(ret)
+        if save:
+            path_list = []
+            for m in tqdm(ret):
+                url = m['url']
+                path = '/'.join(url.split('/')[-5:])
+                filename = '{}/{}'.format(absolute_path(self._save_to), path)
+                Path(filename).parent.mkdir(parents=True, exist_ok=True)
+                r = requests.get(url=url)
+                with open(filename, 'wb') as f:
+                    f.write(r.content)
+                path = filename[:-7]
+                tarfile.open(filename).extractall(path=path)
+                os.remove(filename)
+                path_list.append(path)
+            ret_['save_path'] = path_list
+
+        return ret_.set_index('id', drop=True)

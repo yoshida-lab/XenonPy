69ff14ca86f1cbabea6dfe5bf7ac30e90b3a61f3
tsumina <liu.chang.1865@gmail.com>
2019-08-03 13:55:09 +0900

    using collections.OrderedDict in typing annotation

--- xenonpy/model/training/extension/validator.py
+++ xenonpy/model/training/extension/validator.py
@@ -1,7 +1,8 @@
 #  Copyright (c) 2019. TsumiNa. All rights reserved.
 #  Use of this source code is governed by a BSD-style
 #  license that can be found in the LICENSE file.
-from typing import Callable, Any, Dict, OrderedDict
+from collections import OrderedDict
+from typing import Callable, Any, Dict
 
 import numpy as np
 

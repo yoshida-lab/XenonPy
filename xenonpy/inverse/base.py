46fe1a91d4c340d1034a7a101af19d33909bd394
Stephen Wu <stewu@ism.ac.jp>
2019-09-11 16:51:23 +0900

    Allow multi-likelihood and beta for SMC

--- xenonpy/inverse/base.py
+++ xenonpy/inverse/base.py
@@ -2,12 +2,13 @@
 #  Use of this source code is governed by a BSD-style
 #  license that can be found in the LICENSE file.
 
-from copy import deepcopy
 
 import numpy as np
+import pandas as pd
 from sklearn.base import BaseEstimator
-
 from xenonpy.utils import TimedMetaClass
+from collections import defaultdict
+from collections.abc import Iterable
 
 
 class LogLikelihoodError(Exception):
@@ -53,11 +54,152 @@ class BaseLogLikelihood(BaseEstimator, metaclass=TimedMetaClass):
 
         Returns
         -------
-        log_likelihood: np.ndarray of float
-            Estimated likelihood of each samples.
+        log_likelihood: pd.Dataframe of float (col - properties, row - samples)
+            Estimated log-likelihood of each sample's property values.
+            Cannot be pd.Series!
         """
         # raise NotImplementedError('<log_likelihood> have no implementation')
         raise NotImplementedError('<log_likelihood> method must be implemented')
+
+
+class BaseLogLikelihoodSet(BaseEstimator, metaclass=TimedMetaClass):
+    
+    """
+    Abstract class to organize log-likelihoods.
+    
+    Examples
+    --------
+    .. code::
+        class MyLogLikelihood(BaseLogLikelihoodSet):
+            def __init__(self):
+                super().__init__()
+    
+                self.loglike1 = SomeFeature1()
+                self.loglike1 = SomeFeature2()
+                self.loglike2 = SomeFeature3()
+                self.loglike2 = SomeFeature4()
+    """
+    
+    def __init__(self, *, loglikelihoods='all'):
+        """
+            
+        Parameters
+        ----------
+        loglikelihoods: list[str] or 'all'
+            log-likelihoods that will be used.
+            Default is 'all'.
+        """
+        self.loglikelihoods = loglikelihoods
+        self.__loglikelihoods__ = set()
+        self.__loglikelihood_sets__ = defaultdict(list)
+    
+    @property
+    def elapsed(self):
+        return self._timer.elapsed
+        
+    def __setattr__(self, key, value):
+        
+        if key == '__loglikelihood_sets__':
+            if not isinstance(value, defaultdict):
+                raise RuntimeError('Can not set "self.__loglikelihood_sets__" by yourself')
+            super().__setattr__(key, value)
+        if isinstance(value, BaseLogLikelihood):
+#            if value.__class__.__name__ in self.__loglikelihoods__:
+#                raise RuntimeError('Duplicated log-likelihood <%s>' % value.__class__.__name__)
+            self.__loglikelihood_sets__[key].append(value)
+            self.__loglikelihoods__.add(value.__class__.__name__)
+        else:
+            super().__setattr__(key, value)
+
+    @property
+    def all_loglikelihoods(self):
+        return list(self.__loglikelihoods__)
+    
+    def _check_input(self, X, y=None, **kwargs):
+        def _reformat(x):
+            if x is None:
+                return x
+            
+            keys = list(self.__loglikelihood_sets__.keys())
+            if len(keys) == 1:
+                if isinstance(x, list):
+                    return pd.DataFrame(pd.Series(x), columns=keys)
+                
+                if isinstance(x, np.ndarray):
+                    if len(x.shape) == 1:
+                        return pd.DataFrame(x, columns=keys)
+            
+                if isinstance(x, pd.Series):
+                    return pd.DataFrame(x.values, columns=keys, index=x.index)
+        
+            if isinstance(x, pd.Series):
+                x = pd.DataFrame(x)
+                    
+            if isinstance(x, pd.DataFrame):
+                tmp = set(x.columns) | set(kwargs.keys())
+                if set(keys).isdisjoint(tmp):
+                    raise KeyError(
+                        'name of columns do not match any log-likelihood set')
+                return x
+                
+            raise TypeError(
+                'you can not ues a array-like input'
+                'because there are multiply log-likelihood sets or the dim of input is not 1')
+            
+        return _reformat(X), _reformat(y)
+            
+    def __call__(self, X, **kwargs):
+        return self.log_likelihood(X, **kwargs)
+
+    def log_likelihood(self, X, **kwargs):
+        """
+        Log likelihood
+        
+        Parameters
+        ----------
+        X: list[object]
+            Input samples for likelihood calculation.
+        kwargs: list[string]
+            specified BaseLogLikelihood.
+        
+        Returns
+        -------
+        log_likelihood: pd.Dataframe of float (col - properties, row - samples)
+            Estimated log-likelihood of each sample's property values.
+        """
+        # raise NotImplementedError('<log_likelihood> have no implementation')
+        #raise NotImplementedError('<log_likelihood> method must be implemented')
+            
+        if not isinstance(X, Iterable):
+            raise TypeError('parameter "entries" must be a iterable object')
+                
+        if len(X) is 0:
+            return None
+                        
+        if 'loglikelihoods' in kwargs:
+            loglikelihoods = kwargs['loglikelihoods']
+            if loglikelihoods != 'all' and not isinstance(loglikelihoods, list):
+                raise TypeError('parameter "loglikelihoods" must be a list')
+        else:
+            loglikelihoods = self.loglikelihoods
+                            
+        #if 'return_type' in kwargs:
+        #    del kwargs['return_type']
+
+        results = []
+
+        X, _ = self._check_input(X, **kwargs)
+        for k, lls in self.__loglikelihood_sets__.items():
+            if k in kwargs:
+                k = kwargs[k] # what is this for? even if k not in kwargs... nothing happen right?
+            if k in X:
+                for f in lls:
+                    if loglikelihoods != 'all' and f.__class__.__name__ not in loglikelihoods:
+                        continue
+                    ret = f.log_likelihood(X[k])
+                    results.append(ret)
+                                
+        return pd.concat(results, axis=1)
 
 
 class BaseResample(BaseEstimator, metaclass=TimedMetaClass):
@@ -121,7 +263,8 @@ class BaseSMC(BaseEstimator, metaclass=TimedMetaClass):
     _log_likelihood = None
     _proposal = None
     _resample = None
-    _targets = None
+
+    # _targets = None
 
     def log_likelihood(self, X):
         """
@@ -140,7 +283,8 @@ class BaseSMC(BaseEstimator, metaclass=TimedMetaClass):
         if self._log_likelihood is None:
             raise NotImplementedError('user need to implement <log_likelihood> method or'
                                       'set <self._log_likelihood> to a instance of <BaseLogLikelihood>')
-        return self._log_likelihood(X, **self._targets)
+        # return self._log_likelihood(X, **self._targets)
+        return self._log_likelihood(X)
 
     def resample(self, X, size, p):
         """
@@ -185,7 +329,7 @@ class BaseSMC(BaseEstimator, metaclass=TimedMetaClass):
                                       'set <self._proposal> to a instance of <BaseProposal>')
         return self._proposal(X)
 
-    def on_errors(self, ite, samples, target, error):
+    def on_errors(self, ite, samples, error):
         raise error
 
     def unique(self, X):
@@ -205,41 +349,17 @@ class BaseSMC(BaseEstimator, metaclass=TimedMetaClass):
         """
         return np.unique(X, return_counts=True)
 
-    @property
-    def targets(self):
-        return deepcopy(self._targets)
-
-    def update_targets(self, *, reset=False, **targets):
-        """
-        Update/set the target area.
-
-        Parameters
-        ----------
-        reset: bool
-            If ``true``, reset target area.
-        targets: tuple[float, float]
-            Target area.
-            Should be a tuple which have down and up boundary.
-            e.g: ``target1=(10, 20)`` equal to ``target1 should in range [10, 20]``.
-
-        """
-        if not self._targets or reset:
-            self._targets = {}
-        for k, v in targets.items():
-            if not isinstance(v, tuple) or len(v) != 2 or v[1] <= v[0]:
-                raise ValueError('must be a tuple with (low, up) boundary')
-            self._targets[k] = v
-
     def __setattr__(self, key, value):
-        if key is '_log_likelihood' and not isinstance(value, BaseLogLikelihood):
-            raise TypeError('<self._log_likelihood> must be a subClass of <BaseLogLikelihood>')
+        if key is '_log_likelihood' and not isinstance(value, (BaseLogLikelihood, BaseLogLikelihoodSet)):
+            raise TypeError(
+                '<self._log_likelihood> must be a subClass of <BaseLogLikelihood> or <BaseLogLikelihoodSet>')
         if key is '_proposal' and not isinstance(value, BaseProposal):
             raise TypeError('<self._proposal> must be a subClass of <BaseProposal>')
         if key is '_resample' and not isinstance(value, BaseResample):
             raise TypeError('<self._resample> must be a subClass of <BaseResample>')
         object.__setattr__(self, key, value)
 
-    def __call__(self, samples, beta, *, size=None, yield_lpf=False, **targets):
+    def __call__(self, samples, beta, *, size=None, yield_lpf=False):
         """
         Run SMC
 
@@ -247,17 +367,13 @@ class BaseSMC(BaseEstimator, metaclass=TimedMetaClass):
         ----------
         samples: list of object
             Initial samples.
-        beta: list of float
+        beta: list/1D-numpy of float or pd.Dataframe
             Annealing parameters for each step.
-            Should be a 1-D array-like object.
+            If pd.Dataframe, column names should follow keys of mdl in BaseLogLikeihood or BaseLogLikelihoodSet
         size: int
             Sample size for each draw.
         yield_lpf : bool
             Yield estimated log likelihood, probability and frequency of each samples. Default is ``False``.
-        targets: tuple[float, float]
-            Target area.
-            Should be a tuple which have down and up boundary.
-            e.g: ``target1=(10, 20)`` equal to ``target1 should in range [10, 20]``.
 
         Yields
         -------
@@ -278,19 +394,49 @@ class BaseSMC(BaseEstimator, metaclass=TimedMetaClass):
         if size is None:
             size = len(samples)
 
-        if targets:
-            self.update_targets(**targets)
-        else:
-            if not self._targets:
-                raise ValueError('must set targets area')
+        #         if targets:
+        #             self.update_targets(**targets)
+        #         else:
+        #             if not self._targets:
+        #                 raise ValueError('must set targets area')
+
+        try:
+            unique, frequency = self.unique(samples)
+            ll = self.log_likelihood(unique)
+
+            if isinstance(beta, pd.DataFrame):
+                beta = beta[ll.columns].values
+            else:
+                # assume only one row for beta (1-D list or numpy vector)
+                beta = np.transpose(np.repeat([beta], ll.shape[1], axis=0))
+
+            w = np.dot(ll.values, beta[0]) + np.log(frequency)
+            w_sum = np.log(np.sum(np.exp(w - np.max(w)))) + np.max(w)  # avoid underflow
+            p = np.exp(w - w_sum)
+            if yield_lpf:
+                yield unique, ll, p, frequency
+            else:
+                yield unique
+
+            # beta = np.delete(beta,0,0) #remove first "row"
+
+        except SMCError as e:
+            self.on_errors(0, samples, e)
+        except Exception as e:
+            raise e
 
         # translate between input representation and execute environment representation.
-        for i, step in enumerate(beta):
-            unique, frequency = self.unique(samples)
+        # make sure beta is not changed (np.delete will return the deleted version, without changing original vector)
+        for i, step in enumerate(np.delete(beta, 0, 0)):
             try:
+                re_samples = self.resample(unique, size, p)
+                samples = self.proposal(re_samples)
+
+                unique, frequency = self.unique(samples)
+
                 # annealed likelihood in log - adjust with copy counts
                 ll = self.log_likelihood(unique)
-                w = ll * step + np.log(frequency)
+                w = np.dot(ll.values, step) + np.log(frequency)
                 w_sum = np.log(np.sum(np.exp(w - np.max(w)))) + np.max(w)  # avoid underflow
                 p = np.exp(w - w_sum)
                 if yield_lpf:
@@ -298,24 +444,7 @@ class BaseSMC(BaseEstimator, metaclass=TimedMetaClass):
                 else:
                     yield unique
 
-                re_samples = self.resample(unique, size, p)
-                samples = self.proposal(re_samples)
             except SMCError as e:
-                self.on_errors(i, samples, targets, e)
+                self.on_errors(i + 1, samples, e)
             except Exception as e:
                 raise e
-
-        try:
-            unique, frequency = self.unique(samples)
-            if yield_lpf:
-                ll = self.log_likelihood(unique)
-                w = ll + np.log(frequency)
-                w_sum = np.log(np.sum(np.exp(w - np.max(w)))) + np.max(w)  # avoid underflow
-                p = np.exp(w - w_sum)
-                yield unique, ll, p, frequency
-            else:
-                yield unique
-        except SMCError as e:
-            self.on_errors(i + 1, samples, targets, e)
-        except Exception as e:
-            raise e
--- xenonpy/inverse/iqspr/iqspr.py
+++ xenonpy/inverse/iqspr/iqspr.py
@@ -11,25 +11,19 @@ from xenonpy.inverse.base import BaseSMC, BaseProposal, BaseLogLikelihood
 
 class IQSPR(BaseSMC):
 
-    def __init__(self, *, estimator, modifier, **targets):
+    def __init__(self, *, estimator, modifier):
         """
         SMC iqspr runner.
 
         Parameters
         ----------
-        estimator : BaseLogLikelihood
+        estimator : BaseLogLikelihood or BaseLogLikelihoodSet
             Log likelihood estimator for given SMILES.
         modifier : BaseProposal
             Modify given SMILES to new ones.
-        targets: tuple[float, float]
-            Target area.
-            Should be a tuple which have down and up boundary.
-            e.g: ``target1=(10, 20)`` equal to ``target1 should in range [10, 20]``.
         """
         self._proposal = modifier
         self._log_likelihood = estimator
-        if targets:
-            self.update_targets(**targets)
 
     def resample(self, sims, size, p):
         return np.random.choice(sims, size=size, p=p)

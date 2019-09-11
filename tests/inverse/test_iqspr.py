9baeb0ed37d4854ea6f28a829c473a46e2ec273f
Stephen Wu <stewu@ism.ac.jp>
2019-09-11 20:19:40 +0900

    Allow merging of two NGrams

--- tests/inverse/test_iqspr.py
+++ tests/inverse/test_iqspr.py
@@ -153,6 +153,49 @@ def test_ngram_3(data):
         ngram.proposal(old_smis)
 
 
+def test_ngram_4():
+    smis1 = ['CCCc1ccccc1', 'CC(CCc1ccccc1)CC', 'Cc1ccccc1CC', 'C(CC(C))CC', 'CCCC']
+    n_gram1 = NGram()  # base case
+    n_gram1.fit(smis1, train_order=3)
+    tmp_tab = n_gram1._table
+
+    assert (len(tmp_tab),len(tmp_tab[0][0])) == (3,2)
+
+    check_close = [[(4, 5), (2, 2)], [(6, 5), (3, 2)], [(8, 4), (4, 2)]]
+    check_open = [[(5, 5), (2, 2)], [(6, 5), (3, 2)], [(6, 5), (4, 2)]]
+
+    for ii, x in enumerate(tmp_tab):
+        for i in range(len(x[0])):
+            assert x[0][i].shape == check_close[ii][i]
+            assert x[1][i].shape == check_open[ii][i]
+
+
+def test_ngram_5():
+    smis1 = ['CCCc1ccccc1', 'CC(CCc1ccccc1)CC', 'Cc1ccccc1CC', 'C(CC(C))CC', 'CCCC']
+    smis2 = ['C(F)(F)C', 'CCCF', 'C(F)C=C']
+    smis3 = ['c123c(cc(ccc(N)ccc3)cccc2)cccc1', 'c1cncc1', 'CC(=O)CN']
+    n_gram1 = NGram()  # base case
+    n_gram1.fit(smis1, train_order=3)
+    n_gram2 = NGram()  # higher order but lower num of rings
+    n_gram2.fit(smis2, train_order=4)
+    n_gram3 = NGram()  # lower order but higher num of rings
+    n_gram3.fit(smis3, train_order=2)
+
+    tmp_ngram = n_gram1.merge_table(n_gram2, weight=1, overwrite=False)
+    tmp_tab = tmp_ngram._table
+    assert (len(tmp_tab), len(tmp_tab[0][0])) == (4,2)
+
+    tmp_ngram = n_gram1.merge_table(n_gram3, weight=1, overwrite=False)
+    tmp_tab = tmp_ngram._table
+    assert (len(tmp_tab), len(tmp_tab[0][0])) == (3, 4)
+
+    n_gram1.merge_table(n_gram2, n_gram3, weight=[0.5, 1])
+    tmp_tab = n_gram1._table
+    assert (len(tmp_tab), len(tmp_tab[0][0])) == (4, 4)
+    assert tmp_tab[0][0][0].loc["['C']","C"] == 11.0
+    assert tmp_tab[0][1][0].loc["['(']", "C"] == 3.0
+
+
 def test_iqspr_1(data):
     np.random.seed(0)
     ecfp = ECFP(n_jobs=1, input_type='smiles')

6c66adc0502130433e4bb0fe4b4c6b2bbaa44180
Stephen <39758581+stewu5@users.noreply.github.com>
2019-12-05 16:23:21 +0900

    Update frozen_featurizer.py
    allow control of number of layers to be extracted

--- xenonpy/descriptor/frozen_featurizer.py
+++ xenonpy/descriptor/frozen_featurizer.py
@@ -20,10 +20,9 @@ class FrozenFeaturizer(BaseFeaturizer):
     """
 
     def __init__(self, model: torch.nn.Module = None, *,
-                 cuda: bool = False, depth=None,
+                 cuda: bool = False, depth=None, n_layer=None, 
                  on_errors='raise', return_type='any'):
         """
-
         Parameters
         ----------
         model: torch.nn.Module
@@ -32,6 +31,8 @@ class FrozenFeaturizer(BaseFeaturizer):
             If ``true``, run on GPU.
         depth: int
             The depth will be retrieved from NN model.
+        n_layer: int
+            Number of layer to be retrieved starting from the given depth.
         on_errors: string
             How to handle exceptions in feature calculations. Can be 'nan', 'keep', 'raise'.
             When 'nan', return a column with ``np.nan``.
@@ -47,13 +48,15 @@ class FrozenFeaturizer(BaseFeaturizer):
         """
         super().__init__(n_jobs=0, on_errors=on_errors, return_type=return_type)
         self.depth = depth
+        self.n_layer = n_layer
         self.model = model
         self.cuda = cuda
         self._ret = []
         self.__authors__ = ['TsumiNa']
         self.model.eval()
+        self._depth = 0
 
-    def featurize(self, descriptor, *, depth=None):
+    def featurize(self, descriptor, *, depth=None, n_layer=None):
         if not isinstance(self.model, torch.nn.Module):
             raise TypeError('<model> must be a instance of <torch.nn.Module>')
         hlayers = []
@@ -76,16 +79,42 @@ class FrozenFeaturizer(BaseFeaturizer):
             for m in self.model[:-1]:
                 hlayers.append(m.layer(x_).data)
                 x_ = m(x_)
-
+        
+        # get predefined values if necessary
         if depth is None:
             depth = self.depth
-        if depth is not None:
-            ret = hlayers[-depth:]
-            if depth > len(ret):
-                warnings.warn('<depth> is greater than the max depth of hidden layers')
+        if n_layer is None:
+            n_layer = self.n_layer
+            
+        if n_layer is None:
+            l_end = 0
         else:
-            ret = hlayers
-            self.depth = len(ret)
+            l_end = n_layer-depth
+            
+        self._depth = len(hlayers)
+        if l_end > -1:
+            if l_end > 0:
+                warnings.warn('<n_layer> is over the max depth of hidden layers starting at the given <depth>')
+            if depth is not None:
+                ret = hlayers[-depth:]
+                if depth > len(hlayers):
+                    warnings.warn('<depth> is greater than the max depth of hidden layers')
+                else:
+                    self._depth = depth
+            else:
+                ret = hlayers
+                self.depth = len(hlayers)
+        else:
+            if depth is not None:
+                ret = hlayers[-depth:l_end]
+                if depth > len(hlayers):
+                    warnings.warn('<depth> is greater than the max depth of hidden layers')
+                else:
+                    self._depth = depth
+            else:
+                ret = hlayers[:l_end]
+                self.depth = len(hlayers)
+        
         if self.cuda:
             ret = [l.cpu().numpy() for l in ret]
         else:
@@ -95,9 +124,8 @@ class FrozenFeaturizer(BaseFeaturizer):
 
     @property
     def feature_labels(self):
-        depth = len(self._ret)
-        if depth == 0:
+        if self._depth == 0:
             raise ValueError('Can not generate labels before transform.')
-        return ['L(' + str(i - depth) + ')_' + str(j + 1)
+        return ['L(' + str(i - self._depth) + ')_' + str(j + 1)
                 for i in range(len(self._ret))
                 for j in range(self._ret[i].shape[1])]

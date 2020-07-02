b70efb858e4b92937d276dbedc98c935fe43222a
qi-zh <67253658+qi-zh@users.noreply.github.com>
2020-07-02 13:48:51 +0900

    NGram post-processing (#201)
    * NGram post-processing
    
    Add post-processing to NGram proposal method.
    
    * new_NGram
    
    * 20200626
    
    Co-authored-by: qi-zh <cosmicelon@gmail.com>

---
+++ xenonpy/contrib/extend_NGram/__init__.py
@@ -0,0 +1,8 @@
+#  Copyright (c) 2019. yoshida-lab. All rights reserved.
+#  Use of this source code is governed by a BSD-style
+#  license that can be found in the LICENSE file.
+
+__all__ = ['NewNGram']
+
+
+from .new_ngram import NewNGram

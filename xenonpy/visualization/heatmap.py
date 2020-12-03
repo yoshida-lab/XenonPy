e673d181d713be6cc7841724c770a9d133eedc78
Chang.Liu <TsumiNa@users.noreply.github.com>
2020-12-03 21:37:50 +0900

    Update heatmap.py (#213)
    * Update heatmap.py
    * make return idx optional
    * DescriptorHeatmap can set cmap

--- xenonpy/visualization/heatmap.py
+++ xenonpy/visualization/heatmap.py
@@ -6,7 +6,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import seaborn as sb
-from scipy.stats import boxcox
+from typing import Sequence, Union
+
+from sklearn.preprocessing import power_transform
 from sklearn.base import BaseEstimator
 from sklearn.preprocessing import minmax_scale
 
@@ -19,6 +21,7 @@ class DescriptorHeatmap(BaseEstimator):
     def __init__(self,
                  save=None,
                  bc=False,
+                 cmap='RdBu',
                  pivot_kws=None,
                  method='average',
                  metric='euclidean',
@@ -50,6 +53,7 @@ class DescriptorHeatmap(BaseEstimator):
         mask
         kwargs
         """
+        self.cmap = cmap
         self.save = save
         self.bc = bc
         self.pivot_kws = pivot_kws
@@ -66,51 +70,75 @@ class DescriptorHeatmap(BaseEstimator):
         self.kwargs = kwargs
         self.desc = None
 
-    def _transform(self, series):
-        series_ = series
-        if series.min() != series.max():
-            if self.bc:
-                with np.errstate(all='raise'):
-                    shift = 1e-10
-                    tmp = series - series.min() + shift
-                    try:
-                        series_, _ = boxcox(tmp)
-                    except FloatingPointError:
-                        series_ = series
-        series_ = minmax_scale(series_)
-        return series_
-
     def fit(self, desc):
-        desc_ = desc.apply(self._transform)
-        self.desc = pd.DataFrame(desc_, index=desc.index, columns=desc.columns)
+        desc = minmax_scale(desc, axis=0)
+        if self.bc:
+            desc = power_transform(desc, method='yeo-johnson')
+        self.desc = desc
         return self
 
-    def draw(self, y=None):
-        ax = sb.clustermap(
-            self.desc,
-            cmap="RdBu",
-            method=self.method,
-            figsize=self.figsize,
-            row_cluster=self.row_cluster,
-            col_cluster=self.col_cluster,
-            **self.kwargs)
-        ax.cax.set_visible(False)
-        ax.ax_heatmap.yaxis.set_ticks_position('left')
-        ax.ax_heatmap.yaxis.set_label_position('left')
+    def draw(self, y: Union[Sequence, pd.Series, None] = None, name: str = None, *, return_sorted_idx: bool = False):
+        """
+        Draw figure.
+
+        Parameters
+        ----------
+        y
+            Properties values corresponding to samples.
+        name
+            Property name. If ``name`` is ``None`` and ``y`` is not a ``pandas.Series``.
+            No name will be draw in the figure.
+        return_sorted_idx
+            If ``Ture``, return sorted column index of descriptor.
+            Default ``False``
+
+        Returns
+        -------
+        idx: np.array
+            sorted column index if ``return_sorted_idx`` is ``True``
+
+        """
+        heatmap_ax = sb.clustermap(self.desc,
+                                   cmap=self.cmap,
+                                   method=self.method,
+                                   figsize=self.figsize,
+                                   row_cluster=self.row_cluster,
+                                   col_cluster=self.col_cluster,
+                                   **self.kwargs)
+        heatmap_ax.cax.set_visible(False)
+        heatmap_ax.ax_heatmap.yaxis.set_ticks_position('left')
+        heatmap_ax.ax_heatmap.yaxis.set_label_position('left')
 
         if y is None:
-            ax.ax_col_dendrogram.set_position((0.1, 0.8, 0.9, 0.1))
-            ax.ax_heatmap.set_position((0.1, 0.2, 0.9, 0.6))
+            heatmap_ax.ax_col_dendrogram.set_position((0.1, 0.8, 0.9, 0.1))
+            heatmap_ax.ax_heatmap.set_position((0.1, 0.2, 0.9, 0.6))
         else:
-            ax.ax_col_dendrogram.set_position((0.1, 0.8, 0.83, 0.1))
-            ax.ax_heatmap.set_position((0.1, 0.2, 0.84, 0.6))
-            ax = plt.axes([0.95, 0.2, 0.05, 0.6])
-            x_ = y.values
+            heatmap_ax.ax_col_dendrogram.set_position((0.1, 0.8, 0.83, 0.1))
+            heatmap_ax.ax_heatmap.set_position((0.1, 0.2, 0.84, 0.6))
+            prop_ax = plt.axes([0.95, 0.2, 0.05, 0.6])
+
+            # draw prop
+            if isinstance(y, pd.Series):
+                x_ = y.values
+                name_ = y.name
+            else:
+                x_ = np.asarray(y)
+                name_ = ''
+            if name is not None:
+                name_ = name
             y_ = np.arange(len(x_))[::-1]
-            ax.plot(x_, y_, lw=4)
-            ax.get_yaxis().set_visible(False)
-            ax.spines['top'].set_visible(False)
-            ax.spines['right'].set_visible(False)
-            ax.set_xlabel('{:s}'.format(y.name), fontsize='large')
+
+            prop_ax.plot(x_, y_, lw=4)
+            prop_ax.get_yaxis().set_visible(False)
+            prop_ax.spines['top'].set_visible(False)
+            prop_ax.spines['right'].set_visible(False)
+            prop_ax.set_xlabel('{:s}'.format(name_), fontsize='large')
+
         if self.save:
             plt.savefig(**self.save)
+
+        if return_sorted_idx:
+            try:
+                return heatmap_ax.dendrogram_col.reordered_ind
+            except AttributeError:
+                return np.arange(heatmap_ax.data2d.shape[1])
